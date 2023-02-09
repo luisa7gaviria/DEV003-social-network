58b24ec9aaaa351a0e000b0e377f5efe6061a8c0
Luisa <lorenitaa889@gmail.com>
2023-02-08 20:01:18 -0500

    Bienvenida+Responsive

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { myFunction } from './lib/index.js';
+import { PATHS } from './components/router.js';
 
+console.log(PATHS);
 myFunction();
