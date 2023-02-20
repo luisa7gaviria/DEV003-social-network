7e817d42c145e83a7349032545df2ca343027511
Luisa <lorenitaa889@gmail.com>
2023-02-20 10:09:13 -0500

    validación

--- src/components/create.js
+++ src/components/create.js
@@ -5,14 +5,31 @@ export const create = () => {
   const createComponentContent = `
   <h1 class="create-t">CREAR CUENTA</h1>
   <form id="createForm">
-    <input id= "userName" type="text" name="nombre" placeholder="Nombre" maxlength="16" > 
-    <input id= "userMail" type="text" name="correo" placeholder="Correo Electrónico" maxlength="50" >
-    <input id= "userPassword" type="password" name="contraseña" placeholder="Contraseña" maxlength="16">
-    <input id= "userConfirm" type="password" name="contraseña2" placeholder="Confirma tu contraseña" maxlength="16" >
-    <div > 
-    <button type="submit" id="createAcc" > Crear </button>
+
+   <div id="group__name">
+     <input id= "name" type="text" name="nombre" placeholder="Nombre" maxlength="16" > 
+     <p class="form__input-error" > Este campo solo permite letras y números con un máximo de 16 carácteres. </p>
     </div>
+
+    <div id="group__mail">
+     <input id= "mail" type="text" name="correo" placeholder="Correo Electrónico" maxlength="50" >
+     <p class="form__input-error"> Por favor ingresa una dirección de correo válida. </p>
+    </div>
+
+      <div id="group__password"> 
+         <input id= "password" type="password" name="contraseña" placeholder="Contraseña" maxlength="16">
+         <p class="form__input-error"> Digita de 8 a 16 carácteres incluyendo mayúsculas, minúsculas, números y algún carácter especial sin espacios. </p>
+      </div>
     
+     <div id="group__passwordtwo">
+       <input id= "passwordtwo" type="password" name="contraseña2" placeholder="Confirma tu contraseña" maxlength="16" >
+       <p class="form__input-error"> Las contraseñas deben ser iguales </p>
+     </div>
+
+     <div class="button-create">
+       <button type="submit" id="createAcc" > Crear </button>
+     </div>
+
   </form>
   <button class="back-btn"> Regresar </button>
   `;
@@ -22,21 +39,20 @@ export const create = () => {
 
   const myForm = section.querySelector('#createForm');
   const inputs = section.querySelectorAll('#createForm input');
-  const input1 = section.querySelector('#userPassword');
-  const input2 = section.querySelector('#userConfirm');
+  const input1 = section.querySelector('#password');
+  const input2 = section.querySelector('#passwordtwo');
 
   inputs.forEach((input) => {
     input.addEventListener('keyup', (e) => {
       validateForm(e.target);
-      console.log(validateForm(e.target));
     });
   });
 
   const validatePassword = () => {
     if (input1.value !== input2.value) {
-      console.log('no son iguale');
+      section.querySelector('#group__passwordtwo .form__input-error').classList.add('form__input-error-activo');
     } else {
-      console.log('si son iguale');
+      section.querySelector('#group__passwordtwo .form__input-error').classList.remove('form__input-error-activo');
     }
   };
 
--- src/components/login.js
+++ src/components/login.js
@@ -1,13 +1,22 @@
 import { googleLogIn } from '../lib/auth';
 import { onNavigate } from "../router";
+import { validateForm } from '../validator';
 
 export const login = () => {
   const createLoginContent = `
   <h1 class="login-t"> INICIAR SESIÓN </h1>
     <form id="logForm"> 
-      <input id="inputMail" type="text" name="correo" placeholder="Correo Electrónico"> 
-      <input id="inputPass" type="password" name="contraseña" placeholder="Contraseña">
-      <p>¿Olvidaste tu contraseña?</p>
+
+      <div id="group__mail">
+        <input id="mail" type="text" name="correo" placeholder="Correo Electrónico"> 
+         <p class="form__input-error"> Por favor ingresa una dirección de correo válida. </p>
+      </div>
+
+      <div id="group__password">
+        <input id="password" type="password" name="contraseña" placeholder="Contraseña">
+         <p>¿Olvidaste tu contraseña?</p>
+      </div>
+
       <div>
       <button type="submit" id="enter-button"> Ingresar </button>
       </div>
@@ -16,15 +25,26 @@ export const login = () => {
     <img class="butongoo" src='Images/btngooglein.png'>
 
     <button class="back-btn"> Regresar </button> 
- 
-   <div class="sucess-modal"> 
-      <p> ¡ Ingreso exitoso ! </p>
-      <img class="continue-by-g" src="Images/homelogo.png">
+    
+    <div class="modal"> 
+      <div class="modal-content">
+        <p> ¡ Ingreso exitoso ! Ya puedes continuar a tu muro</p>
+         <img class="continue-by-g" src="Images/homelogo.png">
+        </div>
     </div>
+      
   `;
 
   const loginSection = document.createElement('section');
   loginSection.innerHTML = createLoginContent;
+
+  const loginInputs = loginSection.querySelectorAll('#logForm input');
+
+  loginInputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+      validateForm(e.target);
+    });
+  });
 
   const logForm = loginSection.querySelector('#logForm');
 
@@ -37,7 +57,7 @@ export const login = () => {
     googleLogIn()
       .then(() => {
         console.log('ingreso exitoso');
-        loginSection.querySelector('.sucess-modal').style.display = 'block';
+        loginSection.querySelector('.modal').classList.add('success-modal');
       });
   });
   loginSection.querySelector('.continue-by-g').addEventListener('click', () => {
